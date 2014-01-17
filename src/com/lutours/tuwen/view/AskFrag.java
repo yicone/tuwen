93aec1eb8a761cf78fd14b9dcda3def873afd5c5
yicone <yicone@gmail.com>
2014-01-18 02:58:09 +0800

    answer page enter animation

--- src/com/lutours/tuwen/view/AskFrag.java
+++ src/com/lutours/tuwen/view/AskFrag.java
@@ -1,6 +1,7 @@
 package com.lutours.tuwen.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -10,7 +11,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,40 +26,83 @@ import com.lutours.tuwen.R;
 public class AskFrag extends Fragment {
     private static final int CAMERA_REQUEST = 1888;
     private View rootView;
+
     private ImageView ivPhoto;
+    private EditText etQuestion;
+    private View question_container;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.ask_frag, container, false);
 
         ivPhoto = (ImageView) rootView.findViewById(R.id.ivPhoto);
+        question_container = rootView.findViewById(R.id.question_container);
+        etQuestion = (EditText) rootView.findViewById(R.id.question);
 
+        ivPhoto.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Log.v("ivPhoto", "ivPhoto hasFocus: " + hasFocus);
+            }
+        });
 
-        final View question_container = rootView.findViewById(R.id.question_container);
         question_container.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View view, boolean b) {
-                Log.v("AskFrag", "question_container.setOnFocusChange: " + b);
-                question_container.startAnimation(AnimationUtils.loadAnimation(getActivity(), b ? R.anim.move_up : R.anim.move_down));
+            public void onFocusChange(View v, boolean hasFocus) {
+                Log.v("AskFrag", "question_container hasFocus: " + hasFocus);
             }
         });
 
-        EditText etQuestion = (EditText)rootView.findViewById(R.id.question);
         etQuestion.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View view, boolean b) {
-                Log.v("AskFrag", "etQuestion.setOnFocusChange: " + b);
-                question_container.startAnimation(AnimationUtils.loadAnimation(getActivity(), b ? R.anim.move_up : R.anim.move_down));
+            public void onFocusChange(View v, boolean hasFocus) {
+                Log.v("AskFrag", "question hasFocus: " + hasFocus);
+                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                Animation anim;
+                if (hasFocus) {
+                    imm.showSoftInput(etQuestion, 0);
+                    anim = new TranslateAnimation(0, 0, 0, -60);
+                    anim.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            question_container.setY(0);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+
+                } else {
+                    imm.hideSoftInputFromWindow(etQuestion.getWindowToken(), 0);
+                    anim = new TranslateAnimation(0, 0, -60, 0);
+                    anim.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            question_container.setY(60);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                }
+                question_container.startAnimation(anim);
             }
         });
-
-//        Button btnTestAnim = (Button) rootView.findViewById(R.id.btnTestAnim);
-//        btnTestAnim.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                question_container.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.move_up));
-//            }
-//        });
+        etQuestion.requestFocus();
 
         View camera = rootView.findViewById(R.id.camera);
         camera.setOnClickListener(new View.OnClickListener() {
